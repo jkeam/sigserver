bd38c8966573f9bd5c374c05c99d504c151f1a6e
Jon Keam <jpkeam@gmail.com>
2020-09-22 19:04:20 -0400

    update logging

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const multer = require('multer');
 const winston = require('winston');
 const serveIndex = require('serve-index');
 
+const signaturePath = process.env.SIGNATURE_PATH || './public/signatures';
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/signatures')
+    cb(null, signaturePath);
   },
   filename: (req, file, cb) => {
     // const fileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
@@ -26,17 +27,21 @@ const logger = winston.createLogger({
   transports: [ new winston.transports.Console() ]
 });
 
-app.use(express.static('public'));
-app.use('/signatures', express.static('public'), serveIndex('public/signatures', {'icons': true}));
+app.use(express.static(signaturePath))
+app.use('/signatures', express.static(signaturePath), serveIndex(signaturePath, {'icons': true}));
 
-app.get('/', (req,res) => res.send('Sigserver is up'));
+app.get('/', (req,res) => {
+  logger.log('info', '/');
+  return res.send('Sigserver is up')
+});
 
 app.post('/upload', upload.single('file'), (req,res) => {
+  logger.log('info', '/upload');
   logger.log('debug', `file: ${req.file}`);
   return res.send(req.file);
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   logger.log('info', `Server is up and running on port ${port}`);
 });
