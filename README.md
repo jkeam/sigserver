c88c98490267edba3248196a53408a96e67f90a4
Jon Keam <jpkeam@gmail.com>
2020-09-23 01:12:02 -0400

    add support for binary file

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Server to allow uploading and serving of signatures.
 
 ## Prequisites
 1. Node
+
+## Simple System Test
+
+```
+# start server
+yarn start
+
+# generate binary file
+i=0; while [ $i -lt 256 ]; do echo -en '\x'$(printf "%0x" $i)''  >> binary.dat; i=$((i+1));  done\n
+
+# curl server
+curl --header "Data-Filename: file.dat" --header "Content-Type:application/octet-stream" --location --request POST "http://127.0.0.1:8080/uploadbinary" --data-binary "@./binary.dat"
+
+# verify
+cat ./public/signatures/file.dat
+```
--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const multer = require('multer');
 const winston = require('winston');
 const serveIndex = require('serve-index');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const signaturePath = process.env.SIGNATURE_PATH || './public/signatures';
 const storage = multer.diskStorage({
@@ -19,14 +21,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [ new winston.transports.Console() ]
 });
 
+app.use(bodyParser.raw({type: 'application/octet-stream', limit : '2mb'}))
 app.use(express.static(signaturePath))
 app.use('/signatures', express.static(signaturePath), serveIndex(signaturePath, {'icons': true}));
 
@@ -39,6 +40,18 @@ app.post('/upload', upload.single('file'), (req,res) => {
   logger.log('info', '/upload');
   logger.log('debug', `file: ${req.file}`);
   return res.send(req.file);
+});
+
+app.post('/uploadbinary', (req, res) => {
+  logger.log('info', '/uploadbinary');
+  const filename = req.header('Data-Filename');
+  const filePath = path.join(signaturePath, filename);
+  fs.open(filePath, 'w', (err, fd) => {
+    fs.write(fd, req.body, 0, req.body.length, null, (err) => {
+      if (err) throw err;
+      fs.close(fd, () => res.status(200).end());
+    });
+  });
 });
 
 const port = process.env.PORT || 8080;
