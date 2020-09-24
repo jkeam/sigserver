2b3b6d2b86590ef64cfed84edb9ac38c8416c9ff
Jon Keam <jpkeam@gmail.com>
2020-09-23 23:43:42 -0400

    removed unneeded endpoints and refactor to use middleware logging

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const multer = require('multer');
 const winston = require('winston');
+const expressWinston = require('express-winston');
 const serveIndex = require('serve-index');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const signaturePath = process.env.SIGNATURE_PATH || './public/signatures';
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, signaturePath);
-  },
-  filename: (req, file, cb) => {
-    // const fileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    const fileName = file.originalname;
-    cb(null, fileName);
-  }
-});
-
-const upload = multer({ storage: storage });
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [ new winston.transports.Console() ]
-});
+app.use(expressWinston.logger({
+  transports: [ new winston.transports.Console() ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+}));
 
 app.use(bodyParser.raw({type: 'application/octet-stream', limit : '2mb'}))
 app.use(express.static(signaturePath))
 app.use('/signatures', express.static(signaturePath), serveIndex(signaturePath, {'icons': true}));
 
-app.get('/', (req,res) => {
-  logger.log('info', '/');
-  return res.send('Sigserver is up')
-});
+app.get('/', (req,res) => res.send('Sigserver is up'));
 
-app.post('/upload', upload.single('file'), (req,res) => {
-  logger.log('info', '/upload');
-  logger.log('debug', `file: ${req.file}`);
-  return res.send(req.file);
-});
-
-app.post('/uploadbinary', (req, res) => {
-  logger.log('info', '/uploadbinary');
+app.post('/upload', (req, res) => {
   const filename = req.header('Data-Filename');
   const filePath = path.join(signaturePath, filename);
   fs.open(filePath, 'w', (err, fd) => {
@@ -56,5 +35,5 @@ app.post('/uploadbinary', (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
-  logger.log('info', `Server is up and running on port ${port}`);
+  console.log(`Server is up and running on port ${port}`);
 });
